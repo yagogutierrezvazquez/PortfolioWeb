99c3b9f116ad1c18e88daa939aa63b57e87902ab
yagogutierrezvazquez <github@alias.yagog.dev>
2026-06-03 08:05:14 +0200

    Refactor images code structure for improved readability and maintainability

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -4,12 +4,19 @@
  * This data is consumed by both the Home carousel and the dedicated /certs page.
  */
 
+import ejptImg from '../assets/certificates/ejpt.png';
+import securityPlusImg from '../assets/certificates/security-plus.png';
+import ciscoJrImg from '../assets/certificates/cisco-jr.png';
+import itsCyberImg from '../assets/certificates/its-cyber.png';
+import itsNetwork from '../assets/certificates/its-network.png';
+import sc900Img from '../assets/certificates/sc-900.png';
+
 export interface Certificate {
   name: string;
   issuer: string;
   /** @format "MM YYYY" - Required for i18n date translation logic */
   date: string;
-  image: string;
+  image: ImageMetadata;
   url: string;
 }
 
@@ -23,42 +30,42 @@ export const certificates: Certificate[] = [
     name: "Junior Penetration Tester (eJPT)",
     issuer: "INE Security",
     date: "01 2026",
-    image: "/certificates/ejpt.png",
+    image: ejptImg,
     url: "https://certs.ine.com/9d7755f7-439e-41a6-a978-a8a60e59f65c#acc.fSDbtR1s" 
   },
   {
     name: "CompTIA Security+ (SY0-701)",
     issuer: "CompTIA",
     date: "05 2026",
-    image: "/certificates/security-plus.png",
+    image: securityPlusImg,
     url: "https://www.credly.com/badges/e9025754-c3de-421f-98e5-169eb048f931/public_url"
   },
   {
     name: "IT Specialist - Cybersecurity",
     issuer: "Certiport (Pearson VUE)",
     date: "12 2025",
-    image: "/certificates/its-cyber.png",
+    image: itsCyberImg,
     url: "https://www.credly.com/badges/cc810567-b64d-4a9c-a07a-a325f0a7fda8/public_url"
   },
   {
     name: "IT Specialist - Network Security",
     issuer: "Certiport (Pearson VUE)",
     date: "12 2025",
-    image: "/certificates/its-network.png",
+    image: itsNetwork,
     url: "https://www.credly.com/badges/ff2212c9-d406-4bad-a7d8-bbefe0e11360/public_url"
   },
   {
     name: "Microsoft Certified: SC-900",
     issuer: "Microsoft",
     date: "12 2025",
-    image: "/certificates/sc-900.png",
+    image: sc900Img,
     url: "https://www.credly.com/badges/0c51850d-c77d-4893-a660-8b2e4f064814/public_url"
   },
   {
     name: "Junior Cybersecurity Analyst Career Path",
     issuer: "Cisco",
     date: "03 2024",
-    image: "/certificates/cisco-jr.png",
+    image: ciscoJrImg,
     url: "https://www.credly.com/badges/df64e9f3-cd41-48a8-bd1f-f070e3f57bdc/public_url"
   }
 ];
